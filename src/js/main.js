828487ea6b16a8037691e847f4460239d1d80fa9
Ericevijay12 <ericevijayohiani@gmail.com>
2025-03-20 08:19:18 +0100

    Added alert functionality and styled the alert

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,15 @@ const element = document.querySelector(".product-list")
 const product = new ProductData("tents")
 const productList = new ProductList("tents", product, element)
 productList.init()
+
+document.addEventListener("DOMContentLoaded", function () {
+    const alertBox = document.querySelector(".alert");
+  
+    if (alertBox) {
+      alertBox.style.display = "block"; // Show the alert
+      setTimeout(() => {
+        alertBox.style.display = "none"; // Hide after 5 seconds
+      }, 5000);
+    }
+  });
+  
