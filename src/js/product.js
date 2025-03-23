8c7ce1770d130dbe9ba12c5846653985fe518feb
vickygiovi <vickygiovi@gmail.com>
2025-03-22 23:03:25 -0300

    Version 1.1 Release

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,6 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
