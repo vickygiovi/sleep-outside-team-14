01ed8ce5102bf59351fa4711e2a21675c9a3e32b
vickygiovi <vickygiovi@gmail.com>
2025-03-25 10:06:19 -0300

    External Services

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,10 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
