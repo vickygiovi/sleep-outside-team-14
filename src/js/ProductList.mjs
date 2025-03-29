bf432cb787fa89a25c482627e2930046f763c9a6
ganiyat-bakare <bak24006@byui.edu>
2025-03-29 20:18:05 +0100

    product.mjs updted

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -36,9 +36,14 @@ export default class ProductList {
 
   // Method to render results under different contexts
   renderResults(results) {
-    this.listElement.innerHTML = "";
     // Clear previous results
+    this.listElement.innerHTML = "";
+    if (results.length > 0) {
+      // Render results
     renderListWithTemplate(productCardTemplate, this.listElement, results);
+    } else {
+    this.listElement.innerHTML = "<li>No results found.</li>";
+    }
   }
 
   // render before doing the stretch
